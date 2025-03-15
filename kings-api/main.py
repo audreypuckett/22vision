33b67adaab1b214148305c54225d868d87dfd9f0
Your Name <you@example.com>
2025-03-15 15:20:25 -0500

    base api

---
+++ kings-api/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+# run the application with 'uvicorn main:app --reload'
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
